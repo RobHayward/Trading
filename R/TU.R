1940c8c26ed5e4dec1e417fe21b50b2604c31dcd
RobHayward <robohay@gmail.com>
2015-05-03 21:33:26 +0100

    Updated some more of the matlabl code or TU
    This is mostly about stepping through the code.
    
     Please enter the commit message for your changes. Lines starting

--- R/TU.R
+++ R/TU.R
@@ -13,10 +13,28 @@ da <- da %>%
   mutate(dClose = log(Close) - log(Closel))
 head(da)
 tail(da)
-for(i in 1:dim(da)[1] - lookback){
-  longs <- da$Close[i + lookback] > da$Close[i]
+longs <- rep(NA, dim(da)[1] + 1 - lookback)
+shorts <- rep(NA, dim(da)[1] + 1 - lookback)
+for(i in lookback:dim(da)[1]){
+  longs[i] <- da$Close[i] > da$Close[i + 1 - lookback]
 }
-for(i in 1:dim(da)[1] - lookback){
-  shorts <- da$Close[i + lookback] < da$Close[i]
+for(i in lookback:dim(da)[1]){
+  shorts[i] <- da$Close[i] < da$Close[i + 1 - lookback]
 }
+da <- da[lookback:dim(da)[1],]
+head(da)
+tail(da)
+longs
 # This is the first chapter of Alog trading book.  
+# When long or short is true, add the returns 
+ret <- rep(NA, dim(da)[1])
+for(i in lookback:dim(da)[i]){
+  if(longs == TRUE){
+  ret[i] <- sum(da$dClose[i]:da$dClose[i + holddays] 
+  }
+ if(short == TRUE){
+   ret[i] <- -sum(da$Close[i]:da$Close[i + holddays])
+ }  
+else{
+  ret[i] <- 0
+}
