b962caca0cb530b95b0a4ab13490fee55a986ec9
RobHayward <robohay@gmail.com>
2015-05-07 15:28:44 +0100

    Work on the R.Test for backtesting chapter There has been some work on the Test.R program.  However, this still does not work.  I am not sure why.

--- R/Test.R
+++ R/Test.R
@@ -14,46 +14,25 @@ head(dax)
 dax$ret <- Return.calculate(dax$Close)
 lookback <- 250
 holddays <- 25
-longs <- rep(NA, dim(da)[1] + 1 - lookback - holddays)
-shorts <- rep(NA, dim(da)[1] + 1 - lookback - holddays)
-for(i in lookback + 1 :dim(da)[1]){
-  longs[i] <- da$Close[i] > da$Close[i + 1 - lookback]
+longs <- rep(NA, dim(da)[1] - lookback - holddays)
+shorts <- rep(NA, dim(da)[1] - lookback - holddays)
+for(i in seq(lookback + 1, dim(da)[1] - holddays)){
+  longs[i] <- da$Close[i] > da$Close[i - lookback]
 }
-for(i in lookback + 1:dim(da)[1]){
-  shorts[i] <- da$Close[i] < da$Close[i + 1 - lookback]
+for(i in seq(lookback + 1, dim(da)[1] - holddays)){
+  shorts[i] <- da$Close[i] < da$Close[i - lookback]
 }
-pro <- rep(NA, dim(dax)[1])
-for(i in lookback:dim(dax)[1] - holddays){
-  if (longs[i] == TRUE){
-  pro[i] <- sum(dax$ret[i]:dax$ret[i + holddays], na.rm = TRUE) 
-  } else if(shorts[i] == TRUE){
-   pro[i] <- -sum(dax$Close[i]:dax$Close[i + holddays], na.rm = TRUE)
+pro <- rep(seq(lookback, dim(dax)[1] - holddays))
+for(i in seq(lookback, dim(dax)[1] - holddays)){
+  if (identical(longs[i], TRUE)){
+  pro[i] <- sum(dax$ret[seq(i, i + holddays)], na.rm = TRUE) 
+  } else if(identical(shorts[i], TRUE)){
+   pro[i] <- -sum(dax$Close[seq(i, i + holddays)], na.rm = TRUE)
  } else {
   pro[i] <- 0
 }
 }
-#----------------------
-shorts[lookback +1]
-for(i in lookback + 1: dim(da)[1] - lookback){
-  print(i)
-}
-shorts[lookback +1] <- da$Close[lookback + 1]  < da$Close[lookback + 1 - lookback]
-longs[2] <- da$Close[3] > da$Close[2]
-head(longs)
-ret <- function(start, end){
-  ret[i] <- sum(start:end)
-}
-a <- seq(1, 10, 1)
-for(i in 1:length(a)){
-  if(a[i] == 3){
-print(1)
-}  else if (a[i] ==1){
-print(3)
-}
-else {
-  print(2)
-  k
-}
-str(longs)
-#----------------
+---------------
+  # This does not work.  I am not sure why this does not work. There seem to be e
+  # extra ordinary losses. 
 # Random number generation with randtoolbox package
