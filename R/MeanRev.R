b09905e7860a8859b6ecf3935ec9ded10d4ed69f
RobHayward <robohay@gmail.com>
2015-05-14 10:16:17 +0100

    The difference equation has been estimated. Still needs to sort out the text.  The summary of the regression also needs to be fixed. The calculation of the decahy and other components have to be conciled with the text.  Some additional comments from TIme Series Enders are required. The chapters is nearly compete.

--- R/MeanRev.R
+++ R/MeanRev.R
@@ -1,14 +1,14 @@
 require(Quandl)
-require(dplyr)
+# it is best not to use dplyr as it interferes with vrtest
+# require(dplyr)
 da <- Quandl("BNP/USDCAD",authcode="mUCjthkJFQDsYVrFh4Gh", start_date = "2007-07-01", end_date = "2012-03-31")
 names(da) <- c("Date", "CAD")
 da$lCAD <- log(da$CAD)
-da$llrCAD <- lag(da$lCAD, k = -1)
 nob <- length(da$CAD)
-r <- log(da$CAD[2:nob])-log(da$CAD[1:(nob-1)])
+r <- c(rep(0, nob))
+r[1:nob - 1] <- log(da$CAD[1:nob - 1])-log(da$CAD[2:nob])
+da$r <- r
 # the following is not used because the dplyr package interfers with vrtest
-da <- da %>% 
-  mutate(rCAD =  lCAD - lag(lCAD))
 head(da)
 plot(da$Date, da$CAD, type = 'l', main = "USD/CAD", ylab = "CAD", xlab = "Date")
 library(urca)
@@ -23,11 +23,11 @@ xtable(testtable, caption = "Test statistic and critical values", label = "tab")
 library(pracma)
 a <- hurstexp(da$lCAD)
 library(vrtest)
-b <- Lo.Mac(da$rCAD[2:length(da$CAD)], c(2, 5, 10))
+b <- Lo.Mac(da$r[1:nob - 1], c(2, 5, 10))
 b
-Lo.Mac(r, c(2, 5, 10))
-head(da)
-str(da)
-plot(da$Date, da$rCAD, type = 'l')
-detach(package:dplyr)
-2:nobs
+# Half life test of log change on lagged value.  Half life is -log(2)/beta
+eq <- lm(da$r[1:nob - 1] ~ da$lCAD[2:nob])
+summary(eq)
+HL <- -log(2)/eq$coefficients[2]
+HL
+  
