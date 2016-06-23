73caa09d2eb94c5da9258789558af05726948e37
RobHayward <robohay@gmail.com>
2016-06-23 14:32:36 +0100

    Updated some information on databases.

--- Database/ListOfInstruments.R
+++ Database/ListOfInstruments.R
@@ -6,3 +6,14 @@ theInstruments <- c("^GSPC",
                     "EFA",
                     "EEM",
                     "EWJ")
+# GSPC is S&P 500
+# SPY is the S&P 500 exchange traded fund
+# QQQ is large growth NASDQ 100 index
+# EWJ is MSCI Japan
+# DDM is two times the Dow Jone Industrial Average. 
+# EFA is large and mid-size capitalised developed equity markets (excluding US and Canada).  MSCI EAFE index
+# EWA MSCI Australia. 
+# WEC MSCI Canada
+# GLD gold index
+# IGE:  North America natural resources. 
+# VXX: Vix futures
